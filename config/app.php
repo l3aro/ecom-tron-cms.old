484bc06e17ea940e6c994f78a19edb5f12976d77
l3lackheart <neweragon2@gmail.com>
2018-04-09 14:09:18 +0700

    Add facuz theme package

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Theme
+         */
+        Facuz\Theme\ThemeServiceProvider::class, 
+
     ],
 
     /*
@@ -225,6 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Theme' => Facuz\Theme\Facades\Theme::class,
 
     ],
 
