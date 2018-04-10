fb5bf25a24596d442339e0ff875268c13f1d9896
l3lackheart <dgbao1340@gmail.com>
2018-04-10 16:20:45 +0700

    Layout for other frontend views

---
+++ public/themes/persuit/partials/sections/categories-banner.blade.php
@@ -0,0 +1,14 @@
+<!--================Categories Banner Area =================-->
+<section class="categories_banner_area">
+    <div class="container">
+        <div class="c_banner_inner">
+            <h3>shop grid with left sidebar</h3>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li class="current"><a href="#">Shop Grid with Left Sidebar</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--================End Categories Banner Area =================-->
---
+++ public/themes/persuit/partials/sections/categories-solid.blade.php
@@ -0,0 +1,14 @@
+<!--================Categories Banner Area =================-->
+<section class="solid_banner_area">
+    <div class="container">
+        <div class="solid_banner_inner">
+            <h3>This page</h3>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Those Pages</a></li>
+                <li><a href="#">This Page</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--================End Categories Banner Area =================-->
